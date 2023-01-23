15714f8872878a4304529588c374cc6979050c2b
NikithaMerampally <merampallynikitha@gmail.com>
2023-01-23 14:57:34 +0530

    completed getquery api

--- src/controller/bookController.js
+++ src/controller/bookController.js
@@ -1,6 +1,6 @@
 // ## GET /books/:bookId
 
-const { default: mongoose } = require("mongoose");
+const { isValidObjectId, default: mongoose } = require("mongoose");
 const bookModel = require("../models/books");
 
 exports.getbooks = async (req, res) => {
@@ -18,3 +18,31 @@ exports.getbooks = async (req, res) => {
         res.status(500).json({ status: false, msg: error });
     }
 };
+
+const getBOOksBYQuery=async function(req,res){
+    try{
+    let data=req.query
+    const {userId,category,subCategory}=data
+    if(!userId && !category && !subCategory){
+        let Allbooksdata=await bookModel.find({isDeleted:false}).select({ISBN:0,subcategory:0,deletedAt:0,isDeleted:0,createdAt:0,updatedAt:0}).sort({title:1})
+        return res.status(200).send({status:true,data:Allbooksdata})
+    }
+    if (!isValidObjectId(userId)) {
+        return res.status(400).send({status :false , msg: "Enter A Valid userid" })
+    }
+
+    let book=await bookModel.find({isDeleted:false,...query}).select({ISBN:0,subcategory:0,deletedAt:0,isDeleted:0,createdAt:0,updatedAt:0}).sort({title:1})
+    if(book.length==0) return res.status(404).send({status:false,msg:"books are not found"})
+
+    return res.status(200).send({status:true,data:book})
+
+    }catch (err) {
+    res.status(500).send({ status: false, msg: err.message });
+    }
+    }
+
+    module.exports.getBOOksBYQuery=getBOOksBYQuery;
+
+
+
+

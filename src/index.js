add6d1436304207920a27c9e5c5c36fe8cf4791a
vishanksinghh <109924706+vishanksinghh@users.noreply.github.com>
2023-01-23 12:06:25 +0530

    bookmanagement

---
+++ src/index.js
@@ -0,0 +1,12 @@
+const express=require('express')
+const app=express()
+const route=require('./route/route')
+const mongoose=require('mongoose')
+app.use(express.json())
+mongoose.connect("mongodb+srv://Vishanksingh:7997@cluster0.ga4iiwd.mongodb.net/Ankush1234-DB?retryWrites=true&w=majority",{dbName:"group4Database"},{useNewUrlParser:true}).then(()=>{console.log("mongoDb connected")}).catch((error)=>{console.log(error.message)})
+
+app.use('/',route)
+
+app.listen(3000,()=>{
+    console.log("app is running on port 3000")
+})
